29ed4658b1f00b09e6112a9a774cd41264375377
techguru142 <102968076+techguru142@users.noreply.github.com>
2022-06-09 19:13:35 +0530

    ASf

--- src/controllers/bookController.js
+++ src/controllers/bookController.js
@@ -4,25 +4,26 @@ const publisherModel = require('../models/publisherModel')
 
 const createBook = async function(req, res){
     let bookData = req.body
+    //condition-1
     let authorId = bookData.author_id
-    //Condition-1
-    if(!authorId) res.send("Author id is required")
+    if(!authorId) return res.send("Author id is required")
     //condition-2
-    let pubId = bookData.publisher_id
-    if(!pubId) res.send("Publisher id is required")
-    //condition-3
     let savedAuthData = await authorModel.findById(authorId)
-    if(!savedAuthData) res.send("Invalid author id")
-    //condition-4
+    if(!savedAuthData) return res.send("Invalid author id")
+    //condition-3
+    let pubId = bookData.publisher_id
+    if(!pubId) return res.send("Publisher id is required")
+   //condition-4
     let savedPubData = await publisherModel.findById(pubId)
-    if(!savedPubData) res.send("Invalid publisher id")
+    if(!savedPubData) return res.send("Invalid publisher id")
 
     let savedData = await bookModel.create(bookData)
     res.send(savedData)
 }
 
+
 const getAllBooks = async function(req, res){
-    let savedData = await bookModel.find()
+    let savedData = await bookModel.find().populate('author_id publisher_id')
     res.send(savedData)
 }
 
