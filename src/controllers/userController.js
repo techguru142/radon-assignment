30e9bec7e078b19c7e4ede2a5e6abdcae2ff5a14
techguru142 <102968076+techguru142@users.noreply.github.com>
2022-06-14 21:46:06 +0530

    completed

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -1,14 +1,13 @@
 const jwt = require("jsonwebtoken");
 const userModel = require("../models/userModel");
 
-const createUser = async function (abcd, xyz) {
+const createUser = async function (req, res) {
   //You can name the req, res objects anything.
   //but the first parameter is always the request 
   //the second parameter is always the response
-  let data = abcd.body;
+  let data = req.body;
   let savedData = await userModel.create(data);
-  console.log(abcd.newAtribute);
-  xyz.send({ msg: savedData });
+  res.send({ msg: savedData });
 };
 
 const loginUser = async function (req, res) {
@@ -17,10 +16,7 @@ const loginUser = async function (req, res) {
 
   let user = await userModel.findOne({ emailId: userName, password: password });
   if (!user)
-    return res.send({
-      status: false,
-      msg: "username or the password is not corerct",
-    });
+    return res.send({status: false, msg: "username or the password is not corerct"});
 
   // Once the login is successful, create the jwt token with sign function
   // Sign function has 2 inputs:
@@ -47,7 +43,7 @@ const getUserData = async function (req, res) {
   //If no token is present in the request header return error
   if (!token) return res.send({ status: false, msg: "token must be present" });
 
-  console.log(token);
+  //console.log(token);
   
   // If a token is present then decode the token with verify function
   // verify takes two inputs:
@@ -71,7 +67,9 @@ const updateUser = async function (req, res) {
 // Check if the token is present
 // Check if the token present is a valid token
 // Return a different error message in both these cases
-
+  // let token = req.headers["x- Auth-token"]
+  // if (!token) token = req.headers["x-auth-token"]
+  // if(!token) return res.send({status:false, msg:"token must be present"})
   let userId = req.params.userId;
   let user = await userModel.findById(userId);
   //Return an error if no user with the given id exists in the db
@@ -80,11 +78,24 @@ const updateUser = async function (req, res) {
   }
 
   let userData = req.body;
-  let updatedUser = await userModel.findOneAndUpdate({ _id: userId }, userData);
-  res.send({ status: updatedUser, data: updatedUser });
+  let updatedUser = await userModel.findOneAndUpdate({ _id: userId }, userData,{new:true});
+  res.send({ status: true, data: updatedUser });
 };
+
+const deleteUser = async function(req,res){
+  let userId = req.params.userId;
+  let user = await userModel.findById(userId );
+  if(!user){
+    return res.send("no such user exists");
+  }
+  //let userData = req.body;
+  let deletedUser = await userModel.findOneAndUpdate({_id:userId},{isDeleted:true},{new:true})
+  //console.log(deletedUser)
+  res.send({status:true, msg:deletedUser})
+}
 
 module.exports.createUser = createUser;
 module.exports.getUserData = getUserData;
 module.exports.updateUser = updateUser;
 module.exports.loginUser = loginUser;
+module.exports.deleteUser = deleteUser;
---
+++ src/middlewares/auth.js
@@ -0,0 +1,8 @@
+const checkAuth = function(req,res,next){
+    let token = req.headers["x-Auth-token"]
+  if (!token) token = req.headers["x-auth-token"]
+  if(!token) return res.send({status:false, msg:"token must be present"})
+  next()
+}
+
+module.exports.checkAuth = checkAuth
